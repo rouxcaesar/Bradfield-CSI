88702a94149762b91e9edc13fa8927a7663bb03d
rouxcaesar <jrzerwick@gmail.com>
2021-02-01 21:29:09 -0500

    Refactored ls-clone program to extract processing of files in directory into a separate function

--- prep/c-lang/ls-clone/ls-clone.c
+++ prep/c-lang/ls-clone/ls-clone.c
@@ -2,9 +2,6 @@
 // Minimally, it will list the contents of a directory including
 // some information about each file, such as file size.
 
-// As a stretch goal, use man ls to identify any interesting
-// flags you may wish to support, and implement them.
-
 // Approach:
 // 1) Process argc and argv values to determine correct action(s).
 // 2) If (argc == 1), then we handle the current directory.
@@ -16,14 +13,9 @@
 //    directory (it's just a file).
 // 6) Read the directory contents (contained files/directories).
 
-// Stretch goals:
-// 1) Support -a flag to output hidden files; don't show hidden
-//    files by default.
-// 2) Support -l flat to output more info on files.
-// 3) Sort output of files by name (lexicographic order).
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -31,13 +23,16 @@
 #include <unistd.h>
 #include "dirent.h"
 
+struct flags {
+  bool a;
+};
+
+void print_files(DIR *folder, struct flags f);
+
 int main(int argc, char *argv[]) {
   char *dir;
   DIR *folder;
-  struct dirent *entry;
-  int fd;
-  struct stat buf;
-  off_t size;
+  struct flags f;
 
   // This can be a separate function.
   // Extend into parsing of command args (flags and dir name).
@@ -56,11 +51,27 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
+  print_files(folder, f);
+  closedir(folder);
+
+  return 0;
+}
+
+// print_files takes a DIR instance and considers each file in the DIR.
+// For each file, the function prints its the filesize and name.
+void print_files(DIR *folder, struct flags f) {
+  struct dirent *entry;
+  int fd;
+  struct stat buf;
+  off_t size;
+
   while ((entry = readdir(folder))) {
-    if (((strcmp(entry->d_name, ".")) == 0) || ((strcmp(entry->d_name, "..")) == 0)) {
-      continue;
-    } else if (entry->d_name[0] == '.') {
-      continue;
+    if (!(f.a)) {
+      if (((strcmp(entry->d_name, ".")) == 0) || ((strcmp(entry->d_name, "..")) == 0)) {
+        continue;
+      } else if (entry->d_name[0] == '.') {
+        continue;
+      }
     }
 
     if ((fd = open(entry->d_name, O_RDONLY, 0)) == -1) {
@@ -72,7 +83,4 @@ int main(int argc, char *argv[]) {
     
     printf("%lld\t%s\n", size, entry->d_name);
   }
-
-  closedir(folder);
-  return 0;
 }
