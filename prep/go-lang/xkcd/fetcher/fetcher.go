893b2bfe7cc0737b771f0696174479af9228a9c6
rouxcaesar <jrzerwick@gmail.com>
2021-02-08 21:44:32 -0500

    Added logic to fetch a single comic's JSON payload and output to stdout

--- prep/go-lang/xkcd/fetcher/fetcher.go
+++ prep/go-lang/xkcd/fetcher/fetcher.go
@@ -1,7 +1,28 @@
 package fetcher
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
-func Fetch() {
-	fmt.Printf("Hi from Fetch!\n")
+	"github.com/pkg/errors"
+)
+
+func Fetch() error {
+	fmt.Printf("Hi from Fetch!\n\n")
+
+	// Fetch a comic from xkcd site.
+	resp, err := http.Get("https://xkcd.com/571/info.0.json")
+	if err != nil {
+		return errors.Wrap(err, "failed to make GET request for xkcd comic")
+	}
+
+	// Read JSON payload from response.
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return errors.Wrap(err, "failed to read body of response")
+	}
+
+	fmt.Printf("%s\n\n", data)
+	return nil
 }
--- prep/go-lang/xkcd/main.go
+++ prep/go-lang/xkcd/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"bradfield-csi/prep/go-lang/xkcd/fetcher"
 )
 
 func main() {
-	fmt.Printf("Hi from CSI!\n")
-	fetcher.Fetch()
+	fmt.Printf("Hi from main!\n\n")
+	err := fetcher.Fetch()
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Finished execution, bye!\n")
 }
