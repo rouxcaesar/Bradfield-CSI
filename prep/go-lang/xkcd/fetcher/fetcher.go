9f2adcc9e76e3e2bac3fc76ea28092f90b8fe4fb
rouxcaesar <jrzerwick@gmail.com>
2021-02-08 22:25:14 -0500

    Added logic to create a new file and write the output of each response to the file

--- prep/go-lang/xkcd/fetcher/fetcher.go
+++ prep/go-lang/xkcd/fetcher/fetcher.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/pkg/errors"
 )
@@ -11,18 +12,40 @@ import (
 func Fetch() error {
 	fmt.Printf("Hi from Fetch!\n\n")
 
+	var req string
+
+	f, err := os.Create("index.txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
 	// Fetch a comic from xkcd site.
-	resp, err := http.Get("https://xkcd.com/571/info.0.json")
-	if err != nil {
-		return errors.Wrap(err, "failed to make GET request for xkcd comic")
+	// There are a total of 2422 comics!
+	for i := 1; i <= 2; i++ {
+		req = fmt.Sprintf("https://xkcd.com/%d/info.0.json", i)
+
+		resp, err := http.Get(req)
+		if err != nil {
+			return errors.Wrap(err, "failed to make GET request for xkcd comic")
+		}
+
+		// Read JSON payload from response.
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return errors.Wrap(err, "failed to read body of response")
+		}
+
+		// Append two newline chars to the array to improve the
+		// readability of data stored in file/index.
+		data = append(data, 10)
+		data = append(data, 10)
+
+		_, err = f.Write(data)
+		if err != nil {
+			return err
+		}
 	}
 
-	// Read JSON payload from response.
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return errors.Wrap(err, "failed to read body of response")
-	}
-
-	fmt.Printf("%s\n\n", data)
 	return nil
 }
