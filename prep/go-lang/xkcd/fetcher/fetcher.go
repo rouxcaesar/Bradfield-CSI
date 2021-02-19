43944d309a4acdbd67a42f4bee2041dc34a5463d
rouxcaesar <jrzerwick@gmail.com>
2021-02-18 20:27:07 -0500

    Implemented new version of concurrent URL fetching

--- prep/go-lang/xkcd/fetcher/fetcher.go
+++ prep/go-lang/xkcd/fetcher/fetcher.go
@@ -37,17 +37,13 @@ func Fetch() error {
 	var wg sync.WaitGroup
 	messages := make(chan Comic, maxComics)
 	done := make(chan bool)
+	errs := make(chan error)
+	wgDone := make(chan bool)
 	index := make(map[int]string)
 
-	// Fetch a comic from xkcd site.
-	// There are a total of 2422 comics!
-	//
-	// TODO: Make the fetching of URLs concurrent with
-	//       goroutines and send data through channel.
-	//       Then on other end of channel read data and
-	//       write data to file/index.
 	//for i := 1; i <= maxComics; i++ {
 	for i := 1; i <= 5; i++ {
+		fmt.Printf("In for loop\n\n")
 		// We increment the wait group for each goroutine.
 		wg.Add(1)
 
@@ -59,6 +55,8 @@ func Fetch() error {
 		// involving variable scope and for loops.
 		// Reference: https://dev.to/kkentzo/the-golang-for-loop-gotcha-1n35
 		go func(i int) {
+			fmt.Printf("In producer goroutine %d\n\n", i)
+
 			// We decrement the wait group once the goroutine is finished.
 			defer wg.Done()
 
@@ -67,14 +65,16 @@ func Fetch() error {
 			resp, err := http.Get(req)
 			if err != nil {
 				// Need to send error into error channel
-				return errors.Wrap(err, "failed to make GET request for xkcd comic")
+				errs <- errors.Wrap(err, "failed to make GET request for xkcd comic")
+				//return errors.Wrap(err, "failed to make GET request for xkcd comic")
 			}
 
 			// Read JSON payload from response.
 			data, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
 				// Need to send error into error channel
-				return errors.Wrap(err, "failed to read body of response")
+				errs <- errors.Wrap(err, "failed to read body of response")
+				//return errors.Wrap(err, "failed to read body of response")
 			}
 
 			var comic Comic
@@ -90,11 +90,24 @@ func Fetch() error {
 	//  1) For completion of wait group (new channel and goroutine)
 	//  2) For message from error channel
 	// Block until all of the above goroutines finish.
-	wg.Wait()
+	go func() {
+		fmt.Printf("In wait group goroutine\n\n")
+		wg.Wait()
+		close(wgDone)
+	}()
+
+	select {
+	case <-wgDone:
+		break
+	case e := <-errs:
+		close(errs)
+		return e
+	}
 
 	// Close the channel to signal that we are done using it.
 	// We can do this even before reading message from the channel.
 	close(messages)
+	fmt.Printf("Done with for loop\n\n")
 
 	// This consumer goroutine reads messages off the channel and builds the
 	// index map.
@@ -102,6 +115,7 @@ func Fetch() error {
 	// the producer goroutines update the map.
 	// With this approach, the map is updated in a sequential manner.
 	go func() {
+		fmt.Printf("Inside consumer goroutine\n\n")
 		for c := range messages {
 			index[c.Num] = c.Transcript
 		}
@@ -113,6 +127,8 @@ func Fetch() error {
 	}()
 
 	<-done
+
+	fmt.Printf("About to create file\n\n")
 
 	// Now save the contents of the index variable to a file
 	// to make it an "offline" index.
@@ -127,5 +143,6 @@ func Fetch() error {
 		return errors.Wrap(err, "failed to encode index into index.json")
 	}
 
+	fmt.Printf("Done with Fetcher func!\n\n")
 	return nil
 }
